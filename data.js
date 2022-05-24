c71cfbb913f0c156e8a76bb22449a7adb780d700
mdjhoel <mdjhoel@gmail.com>
2022-05-24 13:39:03 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+data = [{
+	"name": 'Bar road entrance',
+	"desc": "The entrance to the paved section of the Van Horne trail. Don't feed the deer!",
+	"coordinates": [45.09428, -67.05881],
+	"photo": "vanhorne",
+	"rating": "0"
+}, {
+	"name": 'Rest stop',
+	"desc": "Relax and read the rules",
+	"coordinates": [45.090187109010024, -67.05422423381893],
+	"photo": "bench",
+	"rating": "0"
+}, {
+	"name": 'Van Horne trail station',
+	"desc": "Bike tools, plaque to read and a dry place in case of rain.",
+	"coordinates": [45.06989856016825, -67.04375870160882],
+	"photo": "station",
+	"rating": "0"
+}, {
+	"name": 'Accessible bird watching',
+	"desc": "This platform is a great place to view birds and has an accessibility ramp.",
+	"coordinates": [45.07219730803573, -67.041189277033],
+	"photo": "birdwatch",
+	"rating": "0"
+}, {
+	"name": 'Ministers island view',
+	"desc": "At high tide, you can view Ministers island and read the plaque. Come back at low tide to walk or drive across.",
+	"coordinates": [45.099390536466906, -67.05439231172008],
+	"photo": "ministers",
+	"rating": "0"
+}, {
+	"name": 'Passamaquoddy Bay',
+	"desc": "Ride along a coastal beach and wetland with an amazing view of the Bay.",
+	"coordinates": [45.078089094495446, -67.0420167114326],
+	"photo": "bay",
+	"rating": "0"
+}, {
+	"name": 'View from the wharf',
+	"desc": "Check out the boats, sea creatures or the view from the wharf.",
+	"coordinates": [45.07181612943909, -67.05443083399905],
+	"photo": "wharf",
+	"rating": "0"
+}, {
+	"name": 'Aerial view',
+	"desc": "Check out the waterfront, Water street, the Algonquin Hotel, the town and Chamcook Lake.",
+	"coordinates": [45.0707298117314, -67.05240688700147],
+	"photo": "water",
+	"rating": "0"
+}
+]
