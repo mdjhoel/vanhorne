73ef48f7ddb2cc3b688f6a95496f1cd4cc4ff8f4
mdjhoel <mdjhoel@gmail.com>
2022-09-23 18:05:38 +0000

    many locations added

--- data.js
+++ data.js
@@ -24,7 +24,7 @@ data = [{
 	"rating": "0"
 }, {
 	"name": 'Rest stop',
-	"desc": "Relax and read the rules",
+	"desc": "Relax and chill on the bench. Enjoy the peace and quiet of this location.",
 	"coordinates": [45.090187109010024, -67.05422423381893],
 	"photo": "bench",
 	"rating": "0"
@@ -75,6 +75,90 @@ data = [{
 	"desc": "Slow down and enjoy the sites and sounds of our main street. ",
 	"coordinates": [45.07470161133389, -67.05516904592514],
 	"photo": "streetscape",
+	"rating": "0"
+}, {
+	"name": 'Cornelia / 127 intersection',
+	"desc": "Looking south from Cornelia, a calm residential road, to Marine Science drive. Be careful crossing the 127 here.",
+	"coordinates": [45.08928881005657, -67.07134341391287],
+	"photo": "crn127",
+	"rating": "0"
+}, {
+	"name": 'Marine Science intersection',
+	"desc": "Looking south from Marine Science Drive and 127. This is a road with little traffic. Be careful crossing the 127 here.",
+	"coordinates": [45.08884100385681, -67.07108145330186],
+	"photo": "msd127",
+	"rating": "0"
+}, {
+	"name": 'Brandy Cove crosswalk',
+	"desc": "Brandy Cove road connects Marine Science to Jos Point and runs through the Algonquin golf course.",
+	"coordinates": [45.085508634603755, -67.07637255800212],
+	"photo": "xwalkalgo",
+	"rating": "0"
+}, {
+	"name": 'Algonquin golf course',
+	"desc": "This section is one of the hilliest parts of the trail. Watch out for errant golf balls!",
+	"coordinates": [45.08391354812852, -67.07518747683325],
+	"photo": "golftrail",
+	"rating": "0"
+}, {
+	"name": 'Passamaquoddy view',
+	"desc": "The view looking south over the Bay. Take a moment to enjoy one of best views on the trail.",
+	"coordinates": [45.08320461514308, -67.07460794601343],
+	"photo": "golfview",
+	"rating": "0"
+}, {
+	"name": 'Golf course connector',
+	"desc": "Looking north through the Algonquin golf course. Walk up the hill through the golf course to access Marine Science Drive.",
+	"coordinates": [45.08154082634411, -67.07207472766693],
+	"photo": "brandy",
+	"rating": "0"
+}, {
+	"name": 'Pottery Cove at high tide',
+	"desc": "Looking west over the St. Croix river towards the US state of Maine.",
+	"coordinates": [45.07883765479856, -67.06984736333041],
+	"photo": "pottery",
+	"rating": "0"
+}, {
+	"name": 'Blockhouse National Historic site',
+	"desc": "This fort was built to protect Saint Andrews from invasion from the USA during 1812. It worked!",
+	"coordinates": [45.07740589994959, -67.06184158332547],
+	"photo": "nhs",
+	"rating": "0"
+}, {
+	"name": 'Rejoin paved trail',
+	"desc": "The Cemetary road connector ends at the ... cemetary. Turn north onto trail.",
+	"coordinates": [45.08763610337506, -67.05249908331722],
+	"photo": "cemetary",
+	"rating": "0"
+}, {
+	"name": 'One of Canadas great streets',
+	"desc": "In 2016, Water street was recognized by the Canadian Institute of Planners as one of Canadas great streets. We agree!",
+	"coordinates": [45.073659832237354, -67.05292884081781],
+	"photo": "plaque",
+	"rating": "0"
+}, {
+	"name": 'Irish monument',
+	"desc": "Thousands of Irish immigrants came through St Andrews to seek a better life. Unfortunately, many died on Hospital Island, the quarentine location. The Celtic cross on the waterfront commemorates them.",
+	"coordinates": [45.072296109239424, -67.03759871706848],
+	"photo": "celtic",
+	"rating": "0"
+}, {
+	"name": 'Pagan point trail section',
+	"desc": "This paved section of the trail takes you along the Pagan Point Nature Preserve, providing beautiful views of the coastal marsh and Passamaquoddy Bay.",
+	"coordinates": [45.07456619556769, -67.04047504361094],
+	"photo": "entry",
+	"rating": "0"
+}, {
+	"name": 'Katys Cove beach and playground (and washrooms)',
+	"desc": "Stop and relax at a picnic table or take a dip in the warm protected salt water of Katys Cove.",
+	"coordinates": [45.08316691979863, -67.05136000935501],
+	"photo": "washrooms",
+	"rating": "0"
+}, {
+	"name": 'Keep left',
+	"desc": "Traveling north (from town) the trail forks. Turn left to continue on the trail loop, right to go to the (dead end) Ministers Island lookout.",
+	"coordinates": [45.08067845815867, -67.0436936572087],
+	"photo": "left",
 	"rating": "0"
 }
 ]
